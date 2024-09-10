55edf7f5ede3ec4ffdb2c2a836c58e1ce5397c75
Clement <clementyau@wafersystems.com>
2024-09-10 10:27:21 +0800

    update

--- backend/src/main/java/com/example/fp/backend/config/Config.java
+++ backend/src/main/java/com/example/fp/backend/config/Config.java
@@ -3,8 +3,8 @@ package com.example.fp.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class Config {
@@ -20,9 +20,5 @@ public class Config {
         // ObjectMapper mapper = new ObjectMapper();
         // mapper.registerModule(new JavaTimeModule());
         // return mapper;
-    }
-
-    public static void main(String[] args) {
-      System.out.println("Hello");
     }
 }
